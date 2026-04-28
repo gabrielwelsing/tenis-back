cf2443d9f96da895b99faab14e68359554f55d25
gabrielwelsing <gabriel.welsing@gmail.com>
2026-04-28 19:39:11 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { authRouter }     from './routes/authRoutes';
 import { agendaRouter }   from './routes/agenda';
 import { quadrasRouter }  from './routes/quadras';
 import { rankingRouter }  from './routes/ranking';
+import { paymentRouter }  from './routes/paymentRoutes';
 
 const app  = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/health', (_req, res) => res.json({ ok: true }));
+
 app.use('/clips',    clipsRouter);
 app.use('/gabarito', gabaritoRouter);
 app.use('/jogos',    jogosRouter);
@@ -25,5 +27,6 @@ app.use('/auth',     authRouter);
 app.use('/agenda',   agendaRouter);
 app.use('/quadras',  quadrasRouter);
 app.use('/ranking',  rankingRouter);
+app.use('/payment',  paymentRouter);
 
 app.listen(PORT, () => console.log(`tenis-back rodando na porta ${PORT}`));
