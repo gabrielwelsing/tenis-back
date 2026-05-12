d5e9508cb337dc06026f5722c793b76f75fd72c1
gabrielwelsing <gabriel.welsing@gmail.com>
2026-05-12 20:12:57 -0300

    Update paymentRoutes.ts

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -14,7 +14,7 @@ const VALOR_PLANO_CENTAVOS = 1490;
 const PLANO_DIAS = 30;
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
 
 const FRONTEND_URL = process.env.FRONTEND_URL ?? 'https://tenis-to.sup-ia.com';
